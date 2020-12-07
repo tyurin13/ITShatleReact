79fc3ecd2e53c363a9652739b0dc77ac73fa2f6c
tyurin13 <batistabomb2010@gmail.com>
2020-12-07 21:22:23 +0300

    counter

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Container from './commonComponent/Container/Container'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Container />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
